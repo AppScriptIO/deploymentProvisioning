bdbc07fdde9499864bb23b8779d009917e471e48
Safi <nassarsafi@gmail.com>
2019-10-23 00:36:46 +0300

    ⚗ Update exported names and merge module files.

--- source/script.js
+++ source/script.js
@@ -1,10 +1,8 @@
 export * as permission from './terminal/permission.js'
 export * as shellInput from './terminal/shellInput.js'
 
-export * as installJshint from './packageInstallation/nodejsPackage/installJshint.js'
-export * as installPackageUsingJspm from './packageInstallation/nodejsPackage/installPackageUsingJspm.js'
-export * as installPackageUsingNpm from './packageInstallation/nodejsPackage/installPackageUsingNpm.js'
-export * as installPackageUsingYarn from './packageInstallation/nodejsPackage/installPackageUsingYarn.js'
+export * as jshint from './packageInstallation/nodejsPackage/jshint.js'
+export * as installUsingPackageManager from './packageInstallation/nodejsPackage/installUsingPackageManager.js'
 
 export * as docker from './packageInstallation/unixPackage/docker.js'
 export * as git from './packageInstallation/unixPackage/git.js'
