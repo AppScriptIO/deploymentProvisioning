0e9cdbd0d21dcadcd0a57650c1f5f8617302da9a
Safi <nassarsafi@gmail.com>
2020-01-30 14:05:39 +0200

    ⚗ Migrate filesystem operation related functionality to handleFilesystemOperation module. ⚗ Migrate container related functionality to deploymentScript.

--- source/script.js
+++ source/script.js
@@ -12,12 +12,3 @@ export * as yarn from './packageInstallation/unixPackage/yarn.js'
 export * as nodegit from './packageInstallation/unixPackage/nodegit.js'
 export * as nodeVersionManagement from './packageInstallation/unixPackage/nodeVersionManagement.js'
 export * as updateLinux from './packageInstallation/unixPackage/updateLinux.js'
-
-export * as copy from './filesystemOperation/copy.js'
-export * as symlink from './filesystemOperation/symlink.js'
-export * as checkExistence from './filesystemOperation/checkExistence.js'
-export * as synchronize from './filesystemOperation/synchronize.js'
-
-export * as memgraphContainer from './dockerContainer/memgraphContainer.js'
-export * as neo4jContainer from './dockerContainer/neo4jContainer.js'
-export * as redisGraphContainer from './dockerContainer/redisGraphContainer.js'
